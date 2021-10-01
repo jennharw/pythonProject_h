ecc7514f8970e6cb953aa9f25b3b80fce2e59b82
holly0015 <holly001503@gmail.com>
2021-10-01 13:53:32 +0900

    jja

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pickle
+import os
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # if os.path.exists('src/data/graduate.pkl'):
+    #     file = open('src/data/graduate.pkl', 'rb')
+    #     graduate = pickle.load(file)
+    #     file.close()
+    #     graduate.to_csv('src/data/graduate2.csv')
+    #     print('yes')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm BK')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
